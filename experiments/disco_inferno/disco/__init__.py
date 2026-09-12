b44c2abafd679a7c5fee353012c6becdab9f9f14
Nat Osit <nat.a.osit@gmail.com>
2026-09-12 19:29:48 -0400

    Expose DiScO runtime config and feedback APIs

--- experiments/disco_inferno/disco/__init__.py
+++ experiments/disco_inferno/disco/__init__.py
@@ -4,17 +4,32 @@ A Disco Inferno submodel for cheap, deterministic inventories of observable
 text features associated with semantic reconstruction cost.
 """
 
-from .config import DEFAULT_RULES, FeatureRule
+from .config import (
+    DEFAULT_RULES,
+    RUNTIME_RULES_PATH,
+    FeatureRule,
+    load_rules,
+    reset_rules,
+    save_rules,
+)
 from .engine import DiScOProfile, FeatureResult, Match, inspect_text
+from .feedback import FEEDBACK_PATH, load_judgements, record_judgement
 
 DESCRIPTION = "Deterministic Inspection of Semantic Coupling in Outputs"
 
 __all__ = [
     "DESCRIPTION",
     "DEFAULT_RULES",
+    "RUNTIME_RULES_PATH",
+    "FEEDBACK_PATH",
     "DiScOProfile",
     "FeatureResult",
     "FeatureRule",
     "Match",
     "inspect_text",
+    "load_judgements",
+    "load_rules",
+    "record_judgement",
+    "reset_rules",
+    "save_rules",
 ]
